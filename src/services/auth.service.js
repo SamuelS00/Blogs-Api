a42ad74348eca1f549cfaecb14f0f7f274ef5f3d
Samuel <samuelsoutosilva14@gmail.com>
2022-07-14 19:14:34 -0300

    feat(service): add login method

---
+++ src/services/auth.service.js
@@ -0,0 +1,21 @@
+const { User } = require('../database/models/index');
+const { createToken } = require('../helpers/jwt.service');
+const BadRequest = require('../errors/badRequest');
+const { validateLogin } = require('../helpers/validateLogin');
+
+const login = async (email, password) => {
+  validateLogin(email, password);
+
+  const user = await User.findOne(
+    { 
+      where: { email, password },
+      attributes: { exclude: ['password'] },
+    },
+  );
+
+  if (!user) throw BadRequest('Invalid fields');
+
+  return createToken(user);
+};
+
+module.exports = { login };
