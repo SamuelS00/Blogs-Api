9193c751e3ac3e407442f42be4c20156695f7631
Samuel <samuelsoutosilva14@gmail.com>
2022-07-15 20:40:29 -0300

    refacto(user/router): remove validate token

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -3,9 +3,8 @@ const rescue = require('express-rescue');
 
 const userRouter = express.Router();
 const userController = require('../controllers/user.controller');
-const validateToken = require('../middleware/validate.token');
 
-userRouter.get('/', rescue(validateToken), rescue(userController.getAll));
+userRouter.get('/', rescue(userController.getAll));
 userRouter.post('/', rescue(userController.create));
 
 module.exports = userRouter;
