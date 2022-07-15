6986655c127b429055b05733bf0e9b466c258000
Samuel <samuelsoutosilva14@gmail.com>
2022-07-15 17:17:08 -0300

    feat: add reply messages

---
+++ src/helpers/replyMessages.js
@@ -0,0 +1,8 @@
+const replyMessages = {
+  TOKEN_NOT_FOUND: 'Token not found',
+  TOKEN_EXPIRED: 'Expired or invalid token',
+  FIELDS_ARE_MISSING: 'Some required fields are missing',
+  INTERNAL_SERVER_ERROR: 'Internal Server Error',
+};
+
+module.exports = replyMessages;
