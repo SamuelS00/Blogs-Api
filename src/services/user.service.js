d7f57f06b68b5aaac1e06a59b1c5aeee6cea9488
Samuel <samuelsoutosilva14@gmail.com>
2022-07-15 18:01:00 -0300

    refactor: add reply message in method create

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,16 +2,23 @@ const { User } = require('../database/models/index');
 const { createToken } = require('../helpers/jwt.service');
 const { validateNewUser } = require('../helpers/validateBody');
 const Conflict = require('../errors/conflict');
+const { replyMessages } = require('../helpers');
+
+const getAll = async () => {
+  const users = await User.findAll();
+
+  return users;
+};
 
 const create = async (newUser) => {
   validateNewUser(newUser);
   const { email, displayName, image } = newUser;
   const user = await User.findOne({ where: { email } });
 
-  if (user) throw Conflict('User already registered');
+  if (user) throw Conflict(replyMessages.USER_ALREADY_EXISTS);
   await User.create(newUser);  
 
   return createToken(displayName, email, image);
 };
 
-module.exports = { create };
+module.exports = { create, getAll };
