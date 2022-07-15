4f125effc0ce0e946488d320ae1546cef9b01d91
Samuel <samuelsoutosilva14@gmail.com>
2022-07-15 20:01:23 -0300

    feat: add validateToken in route /user GET

--- src/middleware/validate.token.js
+++ src/middleware/validate.token.js
@@ -5,29 +5,30 @@ const { User } = require('../database/models/index');
 const { JWT_SECRET } = process.env;
 const UnauthorizedError = require('../errors/unauthorizedError');
 
-const { httpsStatusCode, replyMessages } = require('../helpers/index');
+const { replyMessages, httpsStatusCode } = require('../helpers/index');
 
 const validateToken = async (req, res, next) => {
     const token = req.headers.authorization;
+    if (!token) throw UnauthorizedError(replyMessages.TOKEN_NOT_FOUND);
 
-    if (!token) return UnauthorizedError(replyMessages.TOKEN_NOT_FOUND);
+    try {
+        const { data } = jwt.verify(token, JWT_SECRET);
 
-    const decoded = jwt.verify(token, JWT_SECRET);
-
-    const user = await User.findOne(
-        { where: { email: decoded.data.user.email } },
-        { attributes: { exclude: ['password'] } },
-    );
-
-    if (!user) {
-        return res.status(httpsStatusCode.UNAUTHORIZED).json(
+        const user = await User.findOne(
+            { where: { email: data.user.email } },
+            { attributes: { exclude: ['password'] } },
+        );
+    
+        if (!user) throw UnauthorizedError(replyMessages.TOKEN_EXPIRED);
+        
+        req.user = user;
+    
+        next();
+    } catch (err) {
+        res.status(httpsStatusCode.UNAUTHORIZED).json(
             { message: replyMessages.TOKEN_EXPIRED },
         );
     }
-
-    req.user = user;
-
-    next();
 };
 
 module.exports = validateToken;
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -3,7 +3,9 @@ const rescue = require('express-rescue');
 
 const userRouter = express.Router();
 const userController = require('../controllers/user.controller');
+const validateToken = require('../middleware/validate.token');
 
+userRouter.get('/', rescue(validateToken), rescue(userController.getAll));
 userRouter.post('/', rescue(userController.create));
 
 module.exports = userRouter;
