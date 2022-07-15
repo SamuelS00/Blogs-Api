a712bb7ff99317e1ba4b79587e325407d17c3138
Samuel <samuelsoutosilva14@gmail.com>
2022-07-15 19:59:59 -0300

    feat(controllers): add method get all

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,17 @@
 const userService = require('../services/user.service');
-const { httpStatusCode } = require('../helpers/index');
+const { httpsStatusCode } = require('../helpers/index');
+
+const getAll = async (req, res, _next) => {
+  const users = await userService.getAll();
+
+  return res.status(httpsStatusCode.OK).json(users);
+};
 
 const create = async (req, res, _next) => {
   const { displayName, email, password, image } = req.body;
   const token = await userService.create({ displayName, email, password, image });
 
-  return res.status(httpStatusCode.ACCEPTED).json({ token });
+  return res.status(httpsStatusCode.CREATED).json({ token });
 };
 
-module.exports = { create };
+module.exports = { create, getAll };
