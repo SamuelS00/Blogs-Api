7cbf1708de52ae2389ce82e46bbc5ca8c5d7c3b2
Samuel <samuelsoutosilva14@gmail.com>
2022-07-15 17:08:19 -0300

    feat(helpers): add index to make it easier

---
+++ src/helpers/index.js
@@ -0,0 +1,5 @@
+const httpsStatusCode = require('./replyStatusCode');
+const replyMessages = require('./replyMessages');
+const validateBody = require('./validateBody');
+
+module.exports = { httpsStatusCode, replyMessages, validateBody };
