00735bfc3b2d989622ba46043432301e6f66c97f
Samuel <samuelsoutosilva14@gmail.com>
2022-07-15 20:00:41 -0300

    fix: import httpStatusCode

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,12 @@
 const authServices = require('../services/auth.service');
-const { httpStatusCode } = require('../helpers/index');
+const { httpsStatusCode } = require('../helpers/index');
 
 const login = async (req, res, _next) => {
   const { email, password } = req.body;
 
   const token = await authServices.login(email, password);
 
-  return res.status(httpStatusCode.OK).json({ token });
+  return res.status(httpsStatusCode.OK).json({ token });
 };
 
 module.exports = { login };
