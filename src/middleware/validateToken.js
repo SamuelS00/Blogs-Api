aaf7121165c554bc5d9153b1c547bb3a86e1bb23
Samuel <samuelsoutosilva14@gmail.com>
2022-07-15 17:18:56 -0300

    feat: add validate token

---
+++ src/middleware/validateToken.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { User } = require('../database/models/index');
+
+const { JWT_SECRET } = process.env;
+const UnauthorizedError = require('../errors/unauthorizedError');
+
+const { httpsStatusCode, replyMessages } = require('../helpers/index');
+
+const validateToken = async (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token) return UnauthorizedError(replyMessages.TOKEN_NOT_FOUND);
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    const user = await User.findOne(
+        { where: { email: decoded.data.user.email } },
+    );
+
+    if (!user) {
+        return res.status(httpsStatusCode.UNAUTHORIZED).json(
+            { message: replyMessages.TOKEN_EXPIRED },
+        );
+    }
+
+    req.user = user;
+
+    next();
+};
+
+module.exports = validateToken;
