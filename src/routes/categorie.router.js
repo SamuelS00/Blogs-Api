c61bf1b5de531e45a3a6d43df0a3a145b8839a4f
Samuel <samuelsoutosilva14@gmail.com>
2022-07-16 16:38:57 -0300

    feat(categories/router): add /categories GET

--- src/routes/categorie.router.js
+++ src/routes/categorie.router.js
@@ -5,6 +5,7 @@ const categorieRouter = express.Router();
 
 const categorieController = require('../controllers/categorie.controller');
 
+categorieRouter.get('/', rescue(categorieController.getAll));
 categorieRouter.post('/', rescue(categorieController.create));
 
 module.exports = categorieRouter;
