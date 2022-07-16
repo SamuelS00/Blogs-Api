7fcb643bb35a1d6c3af14102bd7d9b27b5a8496d
Samuel <samuelsoutosilva14@gmail.com>
2022-07-16 14:40:26 -0300

    refactor(validateBody): add generic function

--- src/helpers/validateBody.js
+++ src/helpers/validateBody.js
@@ -1,39 +1,9 @@
-const Joi = require('joi');
 const BadRequest = require('../errors/badRequest');
-const replyMessages = require('./replyMessages');
 
-const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
-});  
+const validateBody = (schema, body, errMsg = undefined) => {
+  const { error } = schema.validate(body);
 
-const newUserSchema = Joi.object({
-  displayName: Joi.string().min(8).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-  image: Joi.string(),
-});
-
-const newCategorieSchema = Joi.object({
-  name: Joi.string().required(),
-});
-
-const validateLogin = (email, password) => {
-  const { error } = loginSchema.validate({ email, password });
-
-  if (error) throw BadRequest(replyMessages.FIELDS_ARE_MISSING);
+  if (error) throw BadRequest(errMsg || error.message);
 };
 
-const validateNewUser = (user) => {
-  const { error } = newUserSchema.validate(user);
-
-  if (error) throw BadRequest(error.message);
-};
-
-const validateNewCategory = (name) => {
-  const { error } = newCategorieSchema.validate({ name });
-
-  if (error) throw BadRequest(error.message);
-};
-
-module.exports = { validateLogin, validateNewUser, validateNewCategory };
+module.exports = { validateBody };
