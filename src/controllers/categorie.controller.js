31a59a68e737227e3022da40c20501b063c83ed6
Samuel <samuelsoutosilva14@gmail.com>
2022-07-16 16:38:27 -0300

    feat(controller): add method create

--- src/controllers/categorie.controller.js
+++ src/controllers/categorie.controller.js
@@ -2,6 +2,15 @@ const categorieService = require('../services/categorie.service');
 const { validateToken } = require('../services/JWT.service');
 const { httpsStatusCode } = require('../helpers');
 
+const getAll = async (req, res, _next) => {
+  const token = req.headers.authorization;
+  req.user = await validateToken(token);
+
+  const categories = await categorieService.getAll();
+  
+  return res.status(httpsStatusCode.OK).json(categories);
+};
+
 const create = async (req, res, _next) => {
   const token = req.headers.authorization;
   req.user = await validateToken(token);
@@ -12,4 +21,4 @@ const create = async (req, res, _next) => {
   return res.status(httpsStatusCode.CREATED).json(newCategorie);
 };
 
-module.exports = { create };
+module.exports = { create, getAll };
