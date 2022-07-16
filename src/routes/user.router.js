5d328c14ab8c0550f5db28f352722678987166cf
Samuel <samuelsoutosilva14@gmail.com>
2022-07-16 00:41:06 -0300

    feat(user/router): add route /user/:id GET

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -5,6 +5,7 @@ const userRouter = express.Router();
 const userController = require('../controllers/user.controller');
 
 userRouter.get('/', rescue(userController.getAll));
+userRouter.get('/:id', rescue(userController.getById));
 userRouter.post('/', rescue(userController.create));
 
 module.exports = userRouter;
