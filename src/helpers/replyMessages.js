de5f29610ae3188465273fbadb6e6c03b0311431
Samuel <samuelsoutosilva14@gmail.com>
2022-07-16 00:38:37 -0300

    feat: add message from user not exist

--- src/helpers/replyMessages.js
+++ src/helpers/replyMessages.js
@@ -1,10 +1,11 @@
 const replyMessages = {
+  INTERNAL_SERVER_ERROR: 'Internal Server Error',
   TOKEN_NOT_FOUND: 'Token not found',
   TOKEN_EXPIRED: 'Expired or invalid token',
   FIELDS_ARE_MISSING: 'Some required fields are missing',
-  INTERNAL_SERVER_ERROR: 'Internal Server Error',
-  USER_ALREADY_EXISTS: 'User already registered',
   INVALID_FIELDS: 'Invalid fields',
+  USER_ALREADY_EXISTS: 'User already registered',
+  USER_NOT_EXIST: 'User does not exist',
 };
 
 module.exports = replyMessages;
