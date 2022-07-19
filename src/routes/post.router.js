0ae477b6f4439cbaec8c340edd39e9f51dffb1d3
Samuel <samuelsoutosilva14@gmail.com>
2022-07-19 18:52:24 -0300

    feat(router/post): add route /post GET

--- src/routes/post.router.js
+++ src/routes/post.router.js
@@ -2,9 +2,10 @@ const express = require('express');
 const rescue = require('express-rescue');
 
 const postRouter = express.Router();
-
 const postController = require('../controllers/post.controller');
 
 postRouter.post('/', rescue(postController.create));
+
+postRouter.get('/', rescue(postController.getAll));
 
 module.exports = postRouter;
