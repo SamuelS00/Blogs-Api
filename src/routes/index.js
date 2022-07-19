9e77af0dc41c8dac3e7396f5d67370f260f7ccd6
Samuel <samuelsoutosilva14@gmail.com>
2022-07-19 17:28:59 -0300

    feat(index/routes): add export post router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,11 @@
 const authRouter = require('./auth.router');
 const userRouter = require('./user.router');
 const categorieRouter = require('./categorie.router');
+const postRouter = require('./post.router');
 
-module.exports = { authRouter, userRouter, categorieRouter };
+module.exports = { 
+  authRouter,
+  userRouter,
+  categorieRouter,
+  postRouter,
+};
