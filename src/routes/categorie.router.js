bc75e7fcb658631dd574d6fb59a5858c53122905
Samuel <samuelsoutosilva14@gmail.com>
2022-07-20 17:23:01 -0300

    feat(user/router): add router /user/me DELETE

--- src/routes/categorie.router.js
+++ src/routes/categorie.router.js
@@ -2,10 +2,10 @@ const express = require('express');
 const rescue = require('express-rescue');
 
 const categorieRouter = express.Router();
-
 const categorieController = require('../controllers/categorie.controller');
 
 categorieRouter.get('/', rescue(categorieController.getAll));
+
 categorieRouter.post('/', rescue(categorieController.create));
 
 module.exports = categorieRouter;
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -5,7 +5,11 @@ const userRouter = express.Router();
 const userController = require('../controllers/user.controller');
 
 userRouter.get('/', rescue(userController.getAll));
+
 userRouter.get('/:id', rescue(userController.getById));
+
 userRouter.post('/', rescue(userController.create));
+
+userRouter.delete('/me', rescue(userController.destroy));
 
 module.exports = userRouter;
