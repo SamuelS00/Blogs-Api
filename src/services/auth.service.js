26c62f26ba37942d5bdd539d97666c3bd3923a80
Samuel <samuelsoutosilva14@gmail.com>
2022-07-21 10:52:03 -0300

    refactor(auth/service): import from messages

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,16 +1,13 @@
 const { User } = require('../database/models/index');
-const { createToken } = require('./JWT.service');
-const BadRequest = require('../errors/badRequest');
+
+const { createToken } = require('./jwt.service');
+const { BadRequest } = require('../errors/index');
 const { validateBody } = require('../helpers/validateBody');
-const replyMessages = require('../helpers/replyMessages');
+const { FIELDS_ARE_MISSING, INVALID_FIELDS } = require('../helpers/replyMessages');
 const { loginSchema } = require('../utils/joi.schemas');
 
 const login = async (email, password) => {
-  validateBody(
-    loginSchema, 
-    { email, password }, 
-    replyMessages.FIELDS_ARE_MISSING,
-  );
+  validateBody(loginSchema, { email, password }, FIELDS_ARE_MISSING);
 
   const user = await User.findOne(
     { 
@@ -19,7 +16,7 @@ const login = async (email, password) => {
     },
   );
 
-  if (!user) throw BadRequest(replyMessages.INVALID_FIELDS);
+  if (!user) throw BadRequest(INVALID_FIELDS);
 
   return createToken(user);
 };
