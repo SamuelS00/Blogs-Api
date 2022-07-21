2967ca51579846771bf930dc7833f3a91618e523
Samuel <samuelsoutosilva14@gmail.com>
2022-07-21 10:54:44 -0300

    refactor(user/controller):add middlewareValidation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,38 +1,36 @@
 const userService = require('../services/user.service');
-const { httpsStatusCode } = require('../helpers/index');
-const { validateToken } = require('../services/JWT.service');
+const { OK, CREATED, NO_CONTENT } = require('../helpers/replyStatusCode');
 
 const getAll = async (req, res, _next) => {
-  const token = req.headers.authorization;
-  req.user = await validateToken(token);
   const users = await userService.getAll();
 
-  return res.status(httpsStatusCode.OK).json(users);
+  return res.status(OK).json(users);
 };
 
 const getById = async (req, res, _next) => {
-  const token = req.headers.authorization;
   const { id } = req.params;
-  req.user = await validateToken(token);
   const user = await userService.getById(id);
 
-  return res.status(httpsStatusCode.OK).json(user);
+  return res.status(OK).json(user);
 };
 
 const create = async (req, res, _next) => {
   const { displayName, email, password, image } = req.body;
   const token = await userService.create({ displayName, email, password, image });
 
-  return res.status(httpsStatusCode.CREATED).json({ token });
+  return res.status(CREATED).json({ token });
 };
 
 const destroy = async (req, res, _next) => {
-  const token = req.headers.authorization;
-  req.user = await validateToken(token);
   const userId = req.user.dataValues.id;
   await userService.destroy(userId);
 
-  res.status(httpsStatusCode.NO_CONTENT).end();
+  res.status(NO_CONTENT).end();
 };
 
-module.exports = { create, getAll, getById, destroy };
+module.exports = { 
+  create,
+  getAll,
+  getById,
+  destroy,
+};
