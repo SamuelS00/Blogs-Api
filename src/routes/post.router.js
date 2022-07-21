541e43d752a673a0ce04fa797c2c288983dd48ed
Samuel <samuelsoutosilva14@gmail.com>
2022-07-20 22:39:15 -0300

    feat(post/router): add route /post  SEARCH

--- src/routes/post.router.js
+++ src/routes/post.router.js
@@ -4,14 +4,18 @@ const rescue = require('express-rescue');
 const postRouter = express.Router();
 const postController = require('../controllers/post.controller');
 
-postRouter.post('/', rescue(postController.create));
-
 postRouter.get('/', rescue(postController.getAll));
 
+postRouter.get('/search', rescue(postController.search));
+
 postRouter.get('/:id', rescue(postController.getById));
+
+postRouter.post('/', rescue(postController.create));
 
 postRouter.put('/:id', rescue(postController.update));
 
 postRouter.delete('/:id', rescue(postController.destroy));
+
+// postRouter.search('/', rescue(postController.search)); o teste não cobre...
 
 module.exports = postRouter;
