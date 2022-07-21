1b73e7d25855215b655522d532a3d0267520e5da
Samuel <samuelsoutosilva14@gmail.com>
2022-07-21 10:54:30 -0300

    refactor(auth/controller):add middlewareValidation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,11 @@
 const authServices = require('../services/auth.service');
-const { httpsStatusCode } = require('../helpers/index');
+const { OK } = require('../helpers/replyStatusCode');
 
 const login = async (req, res, _next) => {
   const { email, password } = req.body;
-
   const token = await authServices.login(email, password);
 
-  return res.status(httpsStatusCode.OK).json({ token });
+  return res.status(OK).json({ token });
 };
 
 module.exports = { login };
