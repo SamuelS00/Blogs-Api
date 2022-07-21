6d9d29a57565db2e50412a8350b1450d6e81d9e2
Samuel <samuelsoutosilva14@gmail.com>
2022-07-21 10:55:10 -0300

    refactor(user/router):add middlewareValidation

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -3,13 +3,11 @@ const rescue = require('express-rescue');
 
 const userRouter = express.Router();
 const userController = require('../controllers/user.controller');
+const authMiddleware = require('../middleware/auth.middleware');
 
-userRouter.get('/', rescue(userController.getAll));
-
-userRouter.get('/:id', rescue(userController.getById));
-
+userRouter.get('/', authMiddleware, rescue(userController.getAll));
+userRouter.get('/:id', authMiddleware, rescue(userController.getById));
 userRouter.post('/', rescue(userController.create));
-
-userRouter.delete('/me', rescue(userController.destroy));
+userRouter.delete('/me', authMiddleware, rescue(userController.destroy));
 
 module.exports = userRouter;
