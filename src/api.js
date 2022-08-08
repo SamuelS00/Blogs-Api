757fc1c54e4f156c0a5c857c1d320288ad0bbd10
Samuel <samuelsoutosilva14@gmail.com>
2022-08-08 01:28:05 -0300

    docs: add route /api-docs

--- src/api.js
+++ src/api.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
 const errorMiddleware = require('./middleware/error.middleware');
 const { authRouter, userRouter, categorieRouter, postRouter } = require('./routes/index');
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/login', authRouter);
 app.use('/user', userRouter);
